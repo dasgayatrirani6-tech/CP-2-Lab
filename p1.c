d7344eedd4e5c7da5d79cbf2694748da1c4b2759
dasgayatrirani6-tech <dasgayatrirani6@gmail.com>
2026-02-02 17:04:21 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,10 @@
+//Write a program to take a string as input and print it
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char ch[10];
+printf("enter the string");
+gets(ch);
+printf("%s",ch);
+}
---
+++ program5.c
@@ -0,0 +1,11 @@
+//Create a program to input a sentence and display the total number of words
+#include<stdio.h>
+#include<string.h>
+int main();
+{
+	char string[50];
+	printf("enter a sentence");
+	gets(string);
+	printf("%s,"string);
+}
+
