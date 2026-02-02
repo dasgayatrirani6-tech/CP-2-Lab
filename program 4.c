b3b11b23ae465086a151c114b6c1a981f7488dc4
dasgayatrirani6-tech <dasgayatrirani6@gmail.com>
2026-02-02 17:02:49 +0530

    Add files via upload

---
+++ program 4.c
@@ -0,0 +1,12 @@
+//How can you implement a program to count the number of vowels in a string?
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char string[50],i;
+	int count=0;
+	printf("enter a string");
+	gets(string);
+	if(string[i]=='a'||string[i]=='e'||string[i]=='i'||string[i]=='o'||string[i]=='u'||string[i]=='A'||string[i]=='E'||string[i]=='I'||string[i]=='O'||string[i]=='U')
+	printf("%s",string);
+}
---
+++ program2.c
@@ -0,0 +1,12 @@
+//Create a program to input a string and display each character on a new line
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i;
+	char string([i]||0);
+	printf("enter the string");
+	gets(string);
+	for(i=0;i>20;i++)
+	printf("%s\n", string(i));	
+}
